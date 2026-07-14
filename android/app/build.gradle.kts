5ce1b3ec9c98094adfd2a0d2fbd032cd34d0ce2b
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-07-14 13:30:37 +0500

    Enable core library desugaring for local notifications

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,12 +5,17 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+}
+
 android {
     namespace = "com.abdulsamad.medicine_reminder"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true;
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
